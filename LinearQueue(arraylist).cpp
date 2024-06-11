fec8a7d1eb1a114eb84c12e4448181fdb365ddcb
neverleaveualong <wmr06244@naver.com>
2024-06-11 18:11:46 +0900

    no message

--- LinearQueue(arraylist).cpp
+++ LinearQueue(arraylist).cpp
@@ -62,10 +62,12 @@ int dequeue(QueueType* q){
 
 int main(){
     QueueType q;
+
     init_queue(&q);
     enqueue(&q, 10); queue_print(&q);
     enqueue(&q, 20); queue_print(&q);
     enqueue(&q, 30); queue_print(&q);
+    
     dequeue(&q); queue_print(&q);
     dequeue(&q); queue_print(&q);
     dequeue(&q); queue_print(&q);
